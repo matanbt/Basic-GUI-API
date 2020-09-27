226196a397926cb264eb852a86b1eebfaf64c10b
matanbt <63508603+matanbt@users.noreply.github.com>
2020-09-27 19:04:24 +0300

    Update example.py

--- example.py
+++ example.py
@@ -1,16 +1,20 @@
 from GUI_API import GUIApp
 
+
 def test():
-    def foo(x,operator,y):
-        return eval(x+operator+y)
-    app = GUIApp(foo,
-                 output_type='messagebox', title="Basic Calculator")
+    def foo(x, operator, y):
+        return eval(x + operator + y)
+
+    # Initiates a GUI App instance with 'action_func' (the lambda function) and output in pop-up-message
+    app = GUIApp(foo, output_type='messagebox', title="Basic Calculator")
     app.setInnerTitle('Calculator')
-    app.setField('x', 'entry',{'label':'Variable X'})
-    app.setField('y', 'entry',{'label':'Variable Y'})
-    app.setInnerTitle('operator operator operatoroperator'
-                 '')
-    app.setField('operator', 'combobox', {'values': ['+', '-', '*', '/', '**']})
+
+    # Sets the fields, i.e. the arguments of 'action_func'
+    app.setField('x', 'entry', label='Variable X')
+    app.setField('operator', 'combobox', values=['+', '-', '*', '/', '**'])
+    app.setField('y', 'entry', label='Variable Y')
+
+    # opens the GUI App
     app.run()
 
 
